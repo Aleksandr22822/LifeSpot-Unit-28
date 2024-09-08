8129ae3a5d1fc4d97e8c8c98b86f25b7b1e279ca
Aleksandr22822 <alek5andr.bubnov@yandex.ru>
2024-09-08 19:04:48 +0300

    versionForUnit28.3

---
+++ LifeSpot/Static/JS/index.js
@@ -0,0 +1,11 @@
+﻿let ageNewUser = prompt("Сколько Вам лет?");
+if (ageNewUser >= 18) {
+    alert("Приветствуем на LifeSpot! " + new Date().toLocaleString());
+}
+else {
+    alert("К сожалению Вы еще слишком молоды, до свидания!");
+    window.location.href = "https://ya.ru/?npr=1&utm_referrer=https%3A%2F%2Fyandex.ru%2F";
+}
+
+let name = prompt("Как Вас зовут?");
+alert(`Приветствуем, ${name}. В вашем имени ${userName.length} символов`);
